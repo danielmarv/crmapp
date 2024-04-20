20059d32d154728e7fcbd9460e0b923ddada7e45
Daniel Ntege <danientege785@gmail.com>
2024-04-20 19:23:42 +0300

    commit

--- website/views.py
+++ website/views.py
@@ -5,7 +5,6 @@ from django.views.decorators.csrf import csrf_protect
 from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
-@csrf_protect
 def home(request):
     records = Record.objects.all()
     # Check to see if logging in
@@ -20,78 +19,86 @@ def home(request):
             return redirect('home')
         else:
             messages.error(request, "There Was An Error Logging In, Please Try Again...")
-            return redirect('home')
+            return redirect('login')
     else:
         return render(request, 'home.html', {'records': records})
 
-@csrf_protect
 def logout_user(request):
-    logout(request)
-    messages.success(request, "You Have Been Logged Out...")
-    return redirect('home')
+	logout(request)
+	messages.success(request, "You Have Been Logged Out...")
+	return redirect('home')
 
-@csrf_protect
 def register_user(request):
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            form.save()
-            # Authenticate and login
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password1']
-            user = authenticate(username=username, password=password)
-            login(request, user)
-            messages.success(request, "You Have Successfully Registered! Welcome!")
-            return redirect('home')
-    else:
-        form = SignUpForm()
-    return render(request, 'register.html', {'form': form})
+	if request.method == 'POST':
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			# Authenticate and login
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, "You Have Successfully Registered! Welcome!")
+			return redirect('home')
+	else:
+		form = SignUpForm()
+	return render(request, 'register.html', {'form': form})
 
-@csrf_protect
 def customer_record(request, pk):
-    if request.user.is_authenticated:
-        # Look Up Records
-        customer_record = Record.objects.get(id=pk)
-        return render(request, 'record.html', {'customer_record': customer_record})
-    else:
-        messages.error(request, "You Must Be Logged In To View That Page...")
-        return redirect('home')
+	if request.user.is_authenticated:
+		# Look Up Records
+		customer_record = Record.objects.get(id=pk)
+		return render(request, 'record.html', {'customer_record': customer_record})
+	else:
+		messages.error(request, "You Must Be Logged In To View That Page...")
+		return redirect('home')
 
-@csrf_protect
 def delete_record(request, pk):
-    if request.user.is_authenticated:
-        delete_it = Record.objects.get(id=pk)
-        delete_it.delete()
-        messages.success(request, "Record Deleted Successfully...")
-        return redirect('home')
-    else:
-        messages.error(request, "You Must Be Logged In To Do That...")
-        return redirect('home')
+	if request.user.is_authenticated:
+		delete_it = Record.objects.get(id=pk)
+		delete_it.delete()
+		messages.success(request, "Record Deleted Successfully...")
+		return redirect('home')
+	else:
+		messages.error(request, "You Must Be Logged In To Do That...")
+		return redirect('home')
 
-@csrf_protect
 def add_record(request):
-    if request.user.is_authenticated:
-        form = AddRecordForm(request.POST or None)
-        if request.method == "POST":
-            if form.is_valid():
-                add_record = form.save()
-                messages.success(request, "Record Added...")
-                return redirect('home')
-        return render(request, 'add_record.html', {'form': form})
-    else:
-        messages.error(request, "You Must Be Logged In...")
-        return redirect('home')
+	if request.user.is_authenticated:
+		form = AddRecordForm(request.POST or None)
+		if request.method == "POST":
+			if form.is_valid():
+				add_record = form.save()
+				messages.success(request, "Record Added...")
+				return redirect('home')
+		return render(request, 'add_record.html', {'form': form})
+	else:
+		messages.error(request, "You Must Be Logged In...")
+		return redirect('home')
 
-@csrf_protect
+
 def update_record(request, pk):
-    if request.user.is_authenticated:
-        current_record = Record.objects.get(id=pk)
-        form = AddRecordForm(request.POST or None, instance=current_record)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Record Has Been Updated!")
-            return redirect('home')
-        return render(request, 'update_record.html', {'form': form})
-    else:
-        messages.error(request, "You Must Be Logged In...")
-        return redirect('home')
+	if request.user.is_authenticated:
+		current_record = Record.objects.get(id=pk)
+		form = AddRecordForm(request.POST or None, instance=current_record)
+		if form.is_valid():
+			form.save()
+			messages.success(request, "Record Has Been Updated!")
+			return redirect('home')
+		return render(request, 'update_record.html', {'form': form})
+	else:
+		messages.error(request, "You Must Be Logged In...")
+		return redirect('home')
+	
+def user_login(request):
+	if request.method == 'POST':
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			messages.success(request, "You have been logged in successfully!")
+			return redirect('home')  # Assuming 'home' is the name of your home view
+		else:
+			messages.error(request, "Invalid username or password. Please try again.")
+	return render(request, 'login.html')
